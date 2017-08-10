be368b39bcfaaff90fb582bdf95f3a73d9f1876c
LOYCORP\YCAO <ycao@loyalty.com>
2017-08-10 16:30:18 -0400

    API works

--- giveMeSomething/src/apis/tmdbApi.js
+++ giveMeSomething/src/apis/tmdbApi.js
@@ -2,7 +2,7 @@ const API_KEY = 'fdddc52e2ed82bbeaf15dcdf4e2e153e';
 const URL = 'https://api.themoviedb.org/3/discover/movie?';
 const defaultParam = '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 
-const getMovies = () => {
+export const getMovies = () => {
 	let url = URL+ 'api_key=' + API_KEY + defaultParam;
 	return fetch(url)
 	.then(res => res.json())
--- giveMeSomething/src/components/Hello.vue
+++ giveMeSomething/src/components/Hello.vue
@@ -1,33 +1,51 @@
 <template>
+
   <div class='mainPage'>
-    <div class="give">
-       Give me something <input v-model="message1">.
-   </div>
-   <div class="giveHints" v-for="hint in hints">
-     <div v-on:click="copyHint(hint)">{{hint}}</div>
+    <div class="tab">
+    <button v-on:click="iwant=true;ifeel=false;message1=''">I WANT</button>
+    <button  v-on:click="iwant=false; ifeel=true;message2=''">I FEEL</button>
+  </div>
+
+    <div class="iWant" v-show="iwant">
+      <div> Give me something <input v-model="message1">.</div>
+      
+      <div class="giveHints" v-for="hint in hints1">
+        <div v-on:click="copyHint(hint)">{{hint}}</div>
+      </div>
    </div>
 
-   <div class="iFeel">
-       I feel <input v-model="message2"> today.
-   </div>
-   <div class='hints' v-for="hint in hints">
-     <div v-on:click="copyHint(hint)">{{hint}}</div>
+   <div class="iFeel" v-show="ifeel">
+     <div> I feel <input v-model="message2"> today.</div>
+   
+     <div class='hints' v-for="hint in hints2">
+       <div v-on:click="copyHint(hint)">{{hint}}</div>
+     </div>
    </div>
 
-   <button>Find movies</button>
+   <button v-on:click="getMovies()">Find movies</button>
 
 </div>
 
 </template>
 
 <script>
+import {getMovies} from '../apis/tmdbApi';
+
 export default {
   name: 'mainPage',
   data () {
     return {
       message1: '',
       message2: '',
-      hints: [
+      iwant: false,
+      ifeel: true,
+      hints1: [
+      'interesting',
+      'boring',
+      'cool',
+      'depressing'
+      ],
+      hints2: [
       'happy',
       'sad',
       'exited',
@@ -38,9 +56,9 @@ export default {
 
   methods: {
     copyHint(hint) {
-      this.message1 = hint;
-      this.message2 = hint;
-    }
+      this.iwant? this.message1 = hint: this.message2 = hint;
+    },
+    getMovies(){ getMovies(); }
   }
 }
 </script>
